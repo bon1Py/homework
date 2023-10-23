dcf73acce22ba3db446db8bcc57962ed24fb8a80
bon1Py <147813821+bon1Py@users.noreply.github.com>
2023-10-23 19:25:40 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,12 +1,13 @@
-salary = 1500               # зарплата
-spend = 1550                # расходы в месяц
-increase = 1.03             # рост цен в месяц
+salary = 5000               # зарплата
+spend = 6000                # расходы в месяц
+months = 10                 # сколько месяцев без долгов
+increase = 0.03             # рост цен в месяц
 
 money_capital = 0   # счетчик
-for i in range(10) :
+for i in range(months) :
     delta = - (salary - spend)  # долг за текущий месяц
     money_capital += delta      # требуемая фин. подушка к концу текущего месяцев
-    spend = increase * spend    # расходы  на будущий месяц
+    spend += increase * spend    # расходы  на будущий месяц
 
-print('На 10 месяцев фин. подушка должна быть не меньше', round(money_capital, 2), 'ед.')
+print('На', months, 'месяцев фин. подушка должна быть не меньше', round(money_capital, 2), 'ед.')
 print('это', round(money_capital/salary, 1), 'зарплаты')
