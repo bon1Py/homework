c05179bf2b30c9ccf928e8337aef495c852dec2d
bon1Py <147813821+bon1Py@users.noreply.github.com>
2023-10-23 19:24:52 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,13 +1,12 @@
-salary = 1500               # зарплата
-money_capital = 3 * salary  # финансовая подушка
-spend = 1550                # расходы в месяц
-increase = 1.05             # рост цен в месяц
+salary = 5000               # зарплата
+money_capital = 20000       # финансовая подушка
+spend = 6000                # расходы в месяц
+increase = 0.05             # рост цен в месяц
 
 i = 0       # счетчик
 while money_capital > 0 :
     i += 1
-    budget = salary + money_capital - spend  # бюджет к концу i-го месяца
     money_capital = money_capital + (salary - spend)    # подушка к концу i - го месяца
-    spend = increase * spend    # расходы  на месяц (i + 1)
+    spend += increase * spend    # расходы  на месяц (i + 1)
 
 print('Фин. подушки хватит на', i - 1 , 'месяцев')
