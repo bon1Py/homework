3984214d90e57a62c9f010752e0bbf3b253ccb06
bon1Py <147813821+bon1Py@users.noreply.github.com>
2023-10-31 21:04:55 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -9,11 +9,6 @@ def find_common_participants(first_group, second_group):  #
             if second_members == first_members:
                 similar.append(second_members)
 
-    #    for i in range(len(first_group)):
-    #       for j in range(len(second_group)):
-    #            if second_group[j] == first_group[i]:
-    #                similar.append(second_group[j])
-
     similar.sort()  # сортировка по алфавиту
     print("Общие участники среди двух групп -", similar)
 
